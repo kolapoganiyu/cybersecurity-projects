2e446d5d78ee451ec335f29b835d62612b64af3e
Kolapo <kolapsganiyu@gmail.com>
2024-12-10 11:16:02 +0100

    still making changes to the honeyscan

--- Python-projects/HoneyScan.py
+++ Python-projects/HoneyScan.py
@@ -1,1 +1,26 @@
 from scapy.all import *
+
+
+# getting the parameters from the user. The ipaddress and the port numbers
+ip = input("IP ADDRESS -")
+ports = []
+honeys = []
+print("Insert port numbers and press \" ENTER \" in the last prompt")
+
+# working on getting multiple ports
+count = 0
+while True:
+    portNo = input("PORT NO - ")
+    honeysNo = input("HONEY NO -")
+    try:
+        portNo = int(portNo) # converting the string value to integer
+        ports.append(portNo)
+    except:
+        print("Port number must be an integer")
+    if not portNo:
+        if count > 0:
+            break
+    
+    count += 1
+    
+    
