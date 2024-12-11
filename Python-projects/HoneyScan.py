eca242124ab3185f844b51c5beaaf2312f02917c
Kolapo <kolapsganiyu@gmail.com>
2024-12-11 13:19:12 +0100

    Still working on HoneyScan

--- Python-projects/HoneyScan.py
+++ Python-projects/HoneyScan.py
@@ -5,22 +5,56 @@ from scapy.all import *
 ip = input("IP ADDRESS -")
 ports = []
 honeys = []
+bloked = []
 print("Insert port numbers and press \" ENTER \" in the last prompt")
 
 # working on getting multiple ports
-count = 0
-while True:
-    portNo = input("PORT NO - ")
-    honeysNo = input("HONEY NO -")
-    try:
-        portNo = int(portNo) # converting the string value to integer
-        ports.append(portNo)
-    except:
-        print("Port number must be an integer")
+def portsInput():
+    global ports
+    print("insert the port numbers")
+    portNo = input("PORT NO -")
     if not portNo:
-        if count > 0:
-            break
+        return 0
+    portCheck(portNo, ports)
     
-    count += 1
+    
+def honeysInput():
+    global honeys
+    print("insert the honey ports")
+    portNo = input(" HONEY NO -")
+    if not portNo:
+        return 0
+    portCheck(portNo, honeys)
+    
+
+    
+# portCheck will try to convert the value to an integer
+# and appends the resulting value to either the honeys or ports   
+def portCheck(userInput, honeyOrPort):   # 
+    count = 0
+    try:
+        portCheck = int(userInput) # checking if the ports are integer
+        honeyOrPort.append(userInput)
+    except:
+        print("Port number must be integer")
+
+
+print("press \"Enter\" if you do not have any more values to insert")
+while True:
+    portReturnValue = portsInput()
+    honeyReturnValue = honeysInput()
+    
+    if not portReturnValue and not honeyReturnValue:   # if they both have values it will return True and end the loop
+        break
+
+print(ports)
+print(honeys)
+    
+def analyzePackets():
+    global blocked
+    
+    if p.haslayer(IP):
+        pass
+    
     
     
